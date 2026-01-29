1ed9b893d98b2e33a09b83828e681bbd8b5b3d72
pratik-adhau-ppro <pratik.adhau_ext@ppro.com>
2026-01-29 11:56:57 +0530

    enable new blog

--- assets/blogs/blogs.js
+++ assets/blogs/blogs.js
@@ -4,14 +4,13 @@ const blogData = [
         title: 'Building Scalable Microservices',
         date: 'March 2024',
         markdownFile: 'assets/blogs/building-scalable-microservices.md'
+    },
+    {
+        id: 'blog-2',
+        title: 'Mastering Prompt Engineering',
+        date: 'February 2024',
+        markdownFile: 'assets/blogs/mastering-prompt-engineering.md'
     }
-    // ,
-    // {
-    //     id: 'blog-2',
-    //     title: 'Mastering Prompt Engineering',
-    //     date: 'February 2024',
-    //     markdownFile: 'assets/blogs/mastering-prompt-engineering.md'
-    // },
     // {
     //     id: 'blog-3',
     //     title: 'Java vs Scala Performance',
